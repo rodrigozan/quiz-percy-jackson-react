bcf44953af606413225e2cbf98c6e486d51656b7
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2023-04-28 19:59:20 +0000

    Update 9 files

---
+++ src/components/Quiz.js
@@ -0,0 +1,33 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Quiz(props) {
+  const { step, handleAnswer, pergunta, opcoes } = props;
+
+  const renderOpcoes = () => {
+    return opcoes.map((opcao, index) => {
+      return (
+        <div key={index} className="opcoes-grid">
+          <button onClick={() => handleAnswer(opcao)}>{opcao}</button>
+        </div>
+      );
+    });
+  };
+
+  return (
+    <div className="quiz-container">
+      <h2>Pergunta {step}</h2>
+      <h3 className="text-3xl font-bold mb-8">{pergunta}</h3>
+      {renderOpcoes()}
+    </div>
+  );
+}
+
+Quiz.propTypes = {
+  step: PropTypes.number.isRequired,
+  handleAnswer: PropTypes.func.isRequired,
+  pergunta: PropTypes.string.isRequired,
+  opcoes: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default Quiz;
---
+++ src/components/Result.js
@@ -0,0 +1,24 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Result(props) {
+  const { score, totalQuestions, restartQuiz } = props;
+
+  return (
+    <div className="result">
+      <h2>Resultado</h2>
+      <p>
+        Você acertou {score} de {totalQuestions} questões.
+      </p>
+      <button onClick={restartQuiz}>Recomeçar</button>
+    </div>
+  );
+}
+
+Result.propTypes = {
+  score: PropTypes.number.isRequired,
+  totalQuestions: PropTypes.number.isRequired,
+  restartQuiz: PropTypes.func.isRequired,
+};
+
+export default Result;
